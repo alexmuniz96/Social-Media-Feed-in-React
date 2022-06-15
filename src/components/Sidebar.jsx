0c6e0ef42dc932bf6d28d3aef9c59b5e8c02380e
alexmuniz96 <alexmuniz096@gmail.com>
2022-06-15 11:43:54 -0300

    first commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,27 @@
+import { PencilLine } from 'phosphor-react';
+
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src='https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=429&q=50'
+      />
+
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://xesque.rocketseat.dev/users/avatar/profile-66fe409d-ef26-4dec-b3ec-1fde4c2378b3-1624836333736.jpg" />
+        <strong>Alex Muniz</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href='#'>
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
